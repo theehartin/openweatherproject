917b07fb93fd4f5ed77d02aeb6dcb879f92918c7
theehartin <91913346+theehartin@users.noreply.github.com>
2022-01-08 08:04:39 -0600

    Renamed Columns

--- src/main/scala/example/SparkConsumerClass.scala
+++ src/main/scala/example/SparkConsumerClass.scala
@@ -153,6 +153,64 @@ class SparkConsumerClass {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
     var combinedDF = flatDF
     .withColumn("coord", from_json(col("coord"),coordSchema))
     .withColumn("wind",from_json(col("wind"),windSchema))
@@ -226,6 +284,186 @@ class SparkConsumerClass {
     .select("weather.*", "*")
 
     printToConsole(combinedDF5)
+
+ */
+
+
+
+
+
+
+/* 
+
+    var combinedDF2 = flatDF
+    .withColumn("coord", from_json(col("coord"),coordSchema))
+    .withColumn("wind",from_json(col("wind"),windSchema))
+    .withColumn("main",from_json(col("main"),mainSchema))
+    .withColumn("clouds",from_json(col("clouds"),cloudSchema))
+    .withColumn("rain",from_json(col("rain"),rainSchema))
+    .withColumn("snow",from_json(col("snow"),snowSchema))
+    .withColumn("sys",from_json(col("sys"),sysSchema))
+    .withColumn("weather_replace", regexp_replace(col("weather"), "},", "}~"))
+    .select(
+    "coord.*"
+    ,"weather_replace"
+    ,"wind.*"
+    ,"main.*"
+    ,"visibility"
+    ,"clouds"
+    ,"rain" 
+    ,"snow"
+    ,"dt"
+    ,"sys.*"
+    ,"id"
+    ,"name"
+    )
+ */
+
+    var combinedDF2 = flatDF.withColumn("weather_replace", regexp_replace(col("weather"), "},", "}~"))
+
+
+
+/* 
+    var combinedDF3 = combinedDF2
+    .withColumn("weather_replace", regexp_replace(col("weather_replace"), "\\[", ""))
+    .select("*")
+  */   
+
+    var combinedDF3 = combinedDF2.withColumn("weather_replace", regexp_replace(col("weather_replace"), "\\[", ""))
+
+
+/* 
+    var combinedDF4 = combinedDF3
+    .withColumn("weather_split", split(col("weather_replace"), "~" ))
+    .select(
+    explode($"weather_split").as("weather")
+    ,$"*")
+ */
+ 
+    var combinedDF4 = combinedDF3
+    .withColumn("weather_split", split(col("weather_replace"), "~" ))
+    .select(
+    explode($"weather_split").as("weatherTEST")
+    ,$"*")
+
+
+/*     var combinedDF5 = combinedDF4
+    .withColumn("weather_final", from_json(col("weather"), weatherSchema))
+    .select("weather_final.*", "*")
+
+ */
+    var combinedDF5 = combinedDF4
+    .withColumn("weather_final", from_json(col("weatherTEST"), weatherSchema))
+    .withColumn("coord", from_json(col("coord"),coordSchema))
+    .withColumn("wind",from_json(col("wind"),windSchema))
+    .withColumn("main",from_json(col("main"),mainSchema))
+    .withColumn("clouds",from_json(col("clouds"),cloudSchema))
+    .withColumn("rain",from_json(col("rain"),rainSchema))
+    .withColumn("snow",from_json(col("snow"),snowSchema))
+    .withColumn("sys",from_json(col("sys"),sysSchema))
+    .select(
+    col("weather_final.id").as("weather_id")
+    ,col("weather_final.main").as("weather_brief")
+    ,col("weather_final.description").as("weather_description")
+    ,col("coord.lat").as("latidude")
+    ,col("coord.lon").as("longitude")
+    ,col("wind.speed").as("wind_speed")
+    ,col("wind.gust").as("wind_gust")
+    ,col("wind.deg").as("wind_degree")
+    ,col("main.temp").as("temperature")
+    ,col("main.feels_like").as("feels_like_temperature")
+    ,col("main.temp_min").as("minimum_temperature")
+    ,col("main.temp_max").as("maximum_temperature")
+    ,col("main.pressure")
+    ,col("main.humidity")
+    ,col("visibility")
+    ,col("clouds")
+    ,col("rain" )
+    ,col("snow")
+    ,col("sys.sunrise").as("sunrise")
+    ,col("sys.sunset").as("sunset")
+    ,col("id").as("City_id")
+    ,col("name").as("City_name") 
+    )
+
+
+    combinedDF5.printSchema()
+    printToConsole(combinedDF5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /* 
 
